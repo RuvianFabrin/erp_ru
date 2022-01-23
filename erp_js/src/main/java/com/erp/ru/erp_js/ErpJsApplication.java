96c8bea2077dbcb483abe3d14d1e50d8e2af7b08
Ruvian Fabrin <ruvian.fabrin@hotmail.com>
2022-01-23 18:31:03 -0300

    Primeiro Commit

---
+++ erp_js/src/main/java/com/erp/ru/erp_js/ErpJsApplication.java
@@ -0,0 +1,14 @@
+package com.erp.ru.erp_js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpJsApplication.class, args);
+	}
+
+}
+	
